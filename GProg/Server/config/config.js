84fb353baad79f48691a4eef7b5e427b478ae055
bpnowak <bpnowak@uwm.edu>
2018-12-04 15:26:22 -0600

    update

--- GProg/Server/config/config.js
+++ GProg/Server/config/config.js
@@ -5,21 +5,21 @@ var path = require('path'),
 var config = {
     development: {
         root: rootPath,
-        app: { name: 'UCCSS' },
+        app: { name: 'GProg' },
         port: 5000,
         db: 'mongodb://127.0.0.1/helpMe-dev'
 
     },
     test: {
         root: rootPath,
-        app: { name: 'UCCSS' },
+        app: { name: 'GProg' },
         port: 4000,
         db: 'mongodb://127.0.0.1/helpMe-test'
     
     },
     production: {
         root: rootPath,
-        app: { name: 'UCCSS' },
+        app: { name: 'GProg' },
         port: 80,
         db: 'mongodb://127.0.0.1/helpMe'
     }
